8217deb28d16e2eebb9f03af9a44c7b421f33466
DanielRiveros95 <104107281+DanielRiveros95@users.noreply.github.com>
2022-05-08 12:48:29 -0300

    completando ejercicios

--- ejercicios21-04/ejercicio1.ts
+++ ejercicios21-04/ejercicio1.ts
@@ -1,1 +1,18 @@
-console.log("prueba");
+let numero: number = 10;
+
+console.log(numero);
+
+console.log(5 * 7);
+
+console.log(400 / 10);
+
+console.log(33 + 1 * 45);
+
+console.log("calculos");
+
+console.log(numero * 2);
+
+let numero_1: number = 3;
+let numero_2: number = 1;
+let resultado: number = numero_1 + numero_2;
+console.log(resultado);
---
+++ ejercicios21-04/granjerita3.ts
@@ -0,0 +1,10 @@
+console.log("girar a la izquierda");
+console.log("avanzar");
+console.log("girar a la derecha");
+console.log("avanzar");
+console.log("avanzar");
+console.log("avanzar");
+console.log("girar a la derecha");
+console.log("avanzar");
+console.log("quitar monticulo");
+console.log("finalizado");
